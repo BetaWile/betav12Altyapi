e7ee0e1ac784c1177867099d731145cb622d2f2c
B$T4 <77292192+beT4w@users.noreply.github.com>
2021-03-26 20:10:59 +0300

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+const moment = require('moment');
+const chalk = require('chalk');
+const { prefix } = require('../ayarlar.json')
+
+module.exports = client => {
+  var degisenOynuyor = [
+    
+    "Youtube/NoblesYT",
+    "v12 Boş Altyapı"
+    
+  ]
+  
+  setInterval(function() {
+    var degisenOynuyor1 = degisenOynuyor[Math.floor(Math.random() * (degisenOynuyor.length))]
+    client.user.setActivity(`${degisenOynuyor1}`);
+
+}, 2 * 30000);
+  
+  client.user.setStatus("idle"); //dnd, idle, online, offline
+  
+}
